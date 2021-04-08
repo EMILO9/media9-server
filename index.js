950fed49cfbfd8464dd8a635cb0ee80755fb6915
unknown <emilberolsen@gmail.com>
2021-04-08 16:39:02 +0200

    gh

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const mongoClient = require('mongodb').MongoClient
+const objectID = require('mongodb').ObjectID
 
 app.use(express.json())
 
@@ -8,3 +10,14 @@ app.get('/', (req, res)=> {
 })
 
 app.listen(process.env.PORT , () => { console.log("starting...") })
+
+mongoClient.connect(process.env.CONNECTION_STRING, { useUnifiedTopology: true })
+.then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("database");
+    const users = db.collection("users");
+    
+    app.get('/', (req, res)=> {
+        res.send('Hello World')
+    })
+})

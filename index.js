db6fb9739b00692460be8967540db355412896a6
unknown <emilberolsen@gmail.com>
2021-04-09 16:48:52 +0200

    d

--- index.js
+++ index.js
@@ -133,7 +133,10 @@ mongoClient.connect(process.env.CONNECTION_STRING, { useUnifiedTopology: true })
           .then(r => {
             if (!r) res.send("You don't have access to that PC.")
             else {
-              s3.deleteObject({Bucket: process.env.BUCKET, Key: req.params.key})
+              s3.deleteObject({Bucket: process.env.BUCKET, Key: req.params.key}, function(err, data) {
+                if (err) console.log(err, err.stack);
+                else     console.log(data);
+              })
               pcs.updateOne({_id: objectID(req.params.id)},
               { $pull: { media: { key: req.params.key } } }).then(r => {
                 res.send(r)

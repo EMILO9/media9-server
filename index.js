e4162f586348dba7610cb26d3ae2487422b6de45
unknown <emilberolsen@gmail.com>
2021-04-09 09:54:51 +0200

    d

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ const express = require('express')
 const app = express()
 const mongoClient = require('mongodb').MongoClient
 const objectID = require('mongodb').ObjectID
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
+const { body, validationResult } = require('express-validator')
 
 app.use(express.json())
 
 mongoClient.connect(process.env.CONNECTION_STRING, { useUnifiedTopology: true })
 .then((client) => {
-    const db = client.db("db");
-    const users = db.collection("users");
+    const db = client.db("db")
+    const users = db.collection("users")
 
     app.get("/register", (req, res) => {
       let {email, password} = req.body
-      res.send({email, password})
+      body('email').isEmail(),
+      body('password').isLength({ min: 5 }),
+      (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() })
+        else res.send({email, password})
+       }
     })
   })
   
